6a78a3ee94d5b442fc29a56cff772efa121c4529
Your Name <you@example.com>
2021-12-06 15:23:27 +0200

    Initial commit

---
+++ src/test/java/com/academy/realestate1/RealEstate1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.realestate1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstate1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
